c46a7fb9b36ea69d5642fd33117774edb4ccea10
Chae Yungi <l8673901@gmail.com>
2014-01-23 18:33:57 +0900

    SamChon
    Main UI 작업

---
+++ SamChon/MainViewController00.m
@@ -0,0 +1,39 @@
+//
+//  MainViewController00.m
+//  SamChon
+//
+//  Created by SDT-1 on 2014. 1. 23..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import "MainViewController00.h"
+
+@interface MainViewController00 ()
+@property (weak, nonatomic) IBOutlet UIImageView *profileImg;
+
+@end
+
+@implementation MainViewController00
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
