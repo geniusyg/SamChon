1b33c366c193f8bfb9b9f2ff6138999c93d760b8
Chae Yungi <l8673901@gmail.com>
2014-01-24 18:31:24 +0900

    UI작업중..3
    UI작업중..3

---
+++ SamChon/SettingsViewController.m
@@ -0,0 +1,41 @@
+//
+//  SettingsViewController.m
+//  SamChon
+//
+//  Created by SDT-1 on 2014. 1. 24..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+- (IBAction)puchOnOff:(id)sender {
+
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SamChon/WriteViewController.m
@@ -0,0 +1,38 @@
+//
+//  WriteViewController.m
+//  SamChon
+//
+//  Created by SDT-1 on 2014. 1. 24..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import "WriteViewController.h"
+
+@interface WriteViewController ()
+
+@end
+
+@implementation WriteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
